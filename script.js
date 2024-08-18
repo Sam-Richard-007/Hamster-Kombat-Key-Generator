68b74ab4dca49c4b039ca4234f9e8ec6ef73e519
A. Sam Richard <115559742+Sam-Richard-007@users.noreply.github.com>
2024-08-18 07:59:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Riding Extreme 3D',
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            eventsDelay: 20000,
+            eventsDelay: 22000,
             attemptsNumber: 22,
         },
         2: {
