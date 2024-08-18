4d5209445539efc5a8ab9e7bdbd198eaa3e29475
A. Sam Richard <115559742+Sam-Richard-007@users.noreply.github.com>
2024-08-18 07:29:53 +0530

    Update script.js
    Delay fixed for all

--- script.js
+++ script.js
@@ -7,38 +7,43 @@ document.addEventListener('DOMContentLoaded', () => {
             name: 'Riding Extreme 3D',
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            eventsDelay: 22000, // Set the EVENTS_DELAY for BikeRide3D
-  
+            eventsDelay: 20000,
+            attemptsNumber: 22,
         },
         2: {
             name: 'Chain Cube 2048',
             appToken: 'd1690a07-3780-4068-810f-9b5bbf2931b2',
             promoId: 'b4170868-cef0-424f-8eb9-be0622e8e8e3',
-            eventsDelay: 25000, 
+            eventsDelay: 20000,
+            attemptsNumber: 10
         },
         3: {
             name: 'My Clone Army',
             appToken: '74ee0b5b-775e-4bee-974f-63e7f4d5bacb',
             promoId: 'fe693b26-b342-4159-8808-15e3ff7f8767',
             eventsDelay: 70000,
+            attemptsNumber: 11,
         },
         4: {
             name: 'Train Miner',
             appToken: '82647f43-3f87-402d-88dd-09a90025313f',
             promoId: 'c4480ac7-e178-4973-8061-9ed5b2e17954',
-            eventsDelay: 18000,
+            eventsDelay: 20000,
+            attemptsNumber: 10,
         },
         5: {
             name: 'MergeAway',
             appToken: '8d1cc2ad-e097-4b86-90ef-7a27e19fb833',
             promoId: 'dc128d28-c45b-411c-98ff-ac7726fbaea4',
             eventsDelay: 20000,
+            attemptsNumber: 10,
         },
         6: {
     name: 'Twerk Race 3D',
     appToken: '61308365-9d16-4040-8bb0-2f4a4c69074c',
     promoId: '61308365-9d16-4040-8bb0-2f4a4c69074c',
-            eventsDelay: 21000,
+            eventsDelay: 20000,
+        attemptsNumber: 10,
             
         }
     };
