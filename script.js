37fbe833168479adac4d11300300c91651da4801
Sahil Ansari <58244066+imsahilansarii@users.noreply.github.com>
2024-08-21 14:16:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -46,6 +46,13 @@ document.addEventListener('DOMContentLoaded', () => {
             eventsDelay: 20000,
             attemptsNumber: 10,
             
+        },
+        7 : {
+        name: 'Polysphere',
+        appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
+        eventsDelay: 40000,
+        attemptsNumber: 10,
         }
     };
 
