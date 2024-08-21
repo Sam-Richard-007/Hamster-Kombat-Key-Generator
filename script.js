1a8b253ca2c1e5be1c48c5ee0a6288f4a45a067c
A. Sam Richard <115559742+Sam-Richard-007@users.noreply.github.com>
2024-08-21 15:11:40 +0530

    New Game Added

--- script.js
+++ script.js
@@ -51,8 +51,8 @@ document.addEventListener('DOMContentLoaded', () => {
         name: 'Polysphere',
         appToken: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
         promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
-        eventsDelay: 40000,
-        attemptsNumber: 10,
+        eventsDelay: 20000,
+        attemptsNumber: 16,
          }
     };
 
