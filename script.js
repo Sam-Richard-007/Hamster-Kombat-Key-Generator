3d27a00fb9fc46f35a07dbbfdb7dfef1e796ad05
Ali Balaei <carbondev2002@gmail.com>
2024-08-23 22:43:27 +0330

    Add new games
    - Mow and Trim
    - Mud Racing

--- script.js
+++ script.js
@@ -53,6 +53,20 @@ document.addEventListener('DOMContentLoaded', () => {
             promoId: '2aaf5aee-2cbc-47ec-8a3f-0962cc14bc71',
             eventsDelay: 20000,
             // attemptsNumber: 10,
+        },
+        8: {
+            name: 'Mow and Trim',
+            appToken: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            promoId: 'ef319a80-949a-492e-8ee0-424fb5fc20a6',
+            eventsDelay: 20000,
+            // attemptsNumber: 10,
+        },
+        9: {
+            name: 'Mud Racing',
+            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
+            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
+            eventsDelay: 20000,
+            // attemptsNumber: 10,
         }
     };
 
