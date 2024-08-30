37b9c24c917d73abd5ea98f75f45a4754aeea325
A. Sam Richard <115559742+Sam-Richard-007@users.noreply.github.com>
2024-08-30 15:01:33 +0530

    New Games Added

--- script.js
+++ script.js
@@ -11,18 +11,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const games = {
 
         1: {
-
-            name: 'Riding Extreme 3D',
-
-            appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
-
-            promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-
+            name: 'ZooPolis',
+            appToken: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
+            promoId: 'b2436c89-e0aa-4aed-8046-9b0515e1c46b',
             eventsDelay: 21000,
-
-            attemptsNumber: 25,
-
-        },
+            attemptsNumber: 23,
+	},
 
         2: {
 
@@ -118,11 +112,11 @@ document.addEventListener('DOMContentLoaded', () => {
             
 	},
         9: {
-            name: 'Mud Racing ',
-            appToken: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            promoId: '8814a785-97fb-4177-9193-ca4180ff9da8',
-            eventsDelay: 21000,
-            attemptsNumber: 15,
+            name: 'Gangs Wars',
+            appToken: 'b6de60a0-e030-48bb-a551-548372493523',
+            promoId: 'c7821fa7-6632-482c-9635-2bd5798585f9',
+            eventsDelay: 40000,
+            attemptsNumber: 23,
             
 	},
         3: {
